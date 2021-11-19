7c6ed0cddc86cbf7fffe6b96ae4db354c5809cd3
Gang Torchinsky <tor4911@yandex.ru>
2021-11-20 00:26:37 +0300

    Проверка пп на главной/каталоге в адаптиве

--- source/js/modules/filter-show.js
+++ source/js/modules/filter-show.js
@@ -2,16 +2,20 @@ const filterMenu = document.querySelector('.cat-filter');
 const filterBtn = document.querySelector('.cat-filter__btn-open');
 const btnClose = document.querySelector('.cat-filter__btn-close');
 
-filterMenu.classList.add('cat-filter--closed');
+if (filterMenu) {
+  filterMenu.classList.add('cat-filter--closed');
+};
 
-filterBtn.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  filterMenu.classList.remove('cat-filter--closed');
-  body.classList.add('body-lock');
-
-  btnClose.addEventListener('click', () => {
+if (filterBtn) {
+  filterBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
-    filterMenu.classList.add('cat-filter--closed');
-    body.classList.remove('body-lock');
-  })
-});
+    filterMenu.classList.remove('cat-filter--closed');
+    body.classList.add('body-lock');
+  
+    btnClose.addEventListener('click', () => {
+      evt.preventDefault();
+      filterMenu.classList.add('cat-filter--closed');
+      body.classList.remove('body-lock');
+    })
+  });
+};
