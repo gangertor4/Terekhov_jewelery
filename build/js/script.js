77ed6032905f96c54a118612c98af263bf654cee
Gang Torchinsky <tor4911@yandex.ru>
2021-12-02 14:14:01 +0300

    Правки 4.1

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,3 @@
-const accArea = document.querySelectorAll('.accordion');
-const accTitle = document.querySelectorAll('.accordion h3');
-
-if(accArea) {
-  accArea.forEach((area, index) => {
-    area.classList.add('accordion--closed');
-    accTitle[index].addEventListener('click', () => {
-      area.classList.toggle('accordion--closed');
-    });
-  });
-}
-
 const menuBtn = document.querySelector('.header__btn');
 const header = document.querySelector('.header');
 
